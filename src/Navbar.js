1d8d9438296ec0a3bec90738c931f8d51546ea5f
Naveenkumar S E <98598772+SENaveenkumar@users.noreply.github.com>
2022-09-03 10:31:52 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import Box from "./body";
+import './navbar.css';
+
+const MyNav = () => {
+    return (
+        <div>
+        <nav class="navbar">
+            <a class="brand" href="#">RS</a>
+            <ul>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Profile</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <Box /> 
+        </div>
+        
+    )
+}
+
+export default MyNav;
